c8ffd486b3d41b920e0e4f006ac32f29f749de49
neochuky <neochuki@gmail.com>
2012-10-25 21:24:26 +0100

    Timing: Code simplified.

--- timing.rb
+++ timing.rb
@@ -1,10 +1,8 @@
 module Gosu
   
   def self.milliseconds
-    tp = Time.now
-    start = tp.tv_usec / 1000 + tp.tv_sec * 1000
-    res = (tp.tv_usec / 1000 + tp.tv_sec * 1000 - start) & 0x1fffffff
-    res
+    tp = Time.now.to_f
+    (tp*1000).to_i
   end
   
 end
