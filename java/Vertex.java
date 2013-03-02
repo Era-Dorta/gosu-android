e320aa234e24491426c67e83dbfff631154e70f4
neochuky <neochuki@gmail.com>
2013-03-02 16:45:36 +0000

    DrawOp is now an instance variable of Texchunk, with that we give less work to java's garbage collector

--- java/Vertex.java
+++ java/Vertex.java
@@ -18,4 +18,16 @@ public class Vertex{
 		y = y_;
 		c = new Color(c_);
 	}	
+	
+	public void set(float x_, float y_, long c_){
+		x = x_;
+		y = y_;
+		c = new Color(c_);		
+	}
+
+	public void set(float x_, float y_, Color c_){
+		x = x_;
+		y = y_;
+		c = c_;	
+	}
 }
