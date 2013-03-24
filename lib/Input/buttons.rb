9ed08d3429e5f7446d834d9e4180a01e638cbbd5
neochuky <neochuki@gmail.com>
2013-03-24 12:05:42 +0000

    Delete input constants that do not belong to android 2.0. Fixes #6

--- lib/Input/buttons.rb
+++ lib/Input/buttons.rb
@@ -1,5 +1,6 @@
 require 'lib/requires'
 
+
 module Gosu
   Kb0 = JavaImports::KeyEvent::KEYCODE_0
   Kb1 = JavaImports::KeyEvent::KEYCODE_1
@@ -40,49 +41,50 @@ module Gosu
   KbBackspace = JavaImports::KeyEvent::KEYCODE_DEL
   KbDelete = JavaImports::KeyEvent::KEYCODE_DEL
   KbDown = JavaImports::KeyEvent::KEYCODE_DPAD_DOWN 
-  KbEnd = JavaImports::KeyEvent::KEYCODE_MOVE_END
+  #FIXME Only supported on Android 4.0
+  #KbEnd = JavaImports::KeyEvent::KEYCODE_MOVE_END
   # On Numpad
-  KbEnter = JavaImports::KeyEvent::KEYCODE_NUMPAD_ENTER
-  KbEscape = JavaImports::KeyEvent::KEYCODE_ESCAPE
-  KbF1 = JavaImports::KeyEvent::KEYCODE_F1
-  KbF10 = JavaImports::KeyEvent::KEYCODE_F10
-  KbF11 = JavaImports::KeyEvent::KEYCODE_F11
-  KbF12 = JavaImports::KeyEvent::KEYCODE_F12
-  KbF2 = JavaImports::KeyEvent::KEYCODE_F2
-  KbF3 = JavaImports::KeyEvent::KEYCODE_F3
-  KbF4 = JavaImports::KeyEvent::KEYCODE_F4
-  KbF5 = JavaImports::KeyEvent::KEYCODE_F5
-  KbF6 = JavaImports::KeyEvent::KEYCODE_F6
-  KbF7 = JavaImports::KeyEvent::KEYCODE_F7
-  KbF8 = JavaImports::KeyEvent::KEYCODE_F8
-  KbF9 = JavaImports::KeyEvent::KEYCODE_F9
+  #KbEnter = JavaImports::KeyEvent::KEYCODE_NUMPAD_ENTER
+  #KbEscape = JavaImports::KeyEvent::KEYCODE_ESCAPE
+  #KbF1 = JavaImports::KeyEvent::KEYCODE_F1
+  #KbF10 = JavaImports::KeyEvent::KEYCODE_F10
+  #KbF11 = JavaImports::KeyEvent::KEYCODE_F11
+  #KbF12 = JavaImports::KeyEvent::KEYCODE_F12
+  #KbF2 = JavaImports::KeyEvent::KEYCODE_F2
+  #KbF3 = JavaImports::KeyEvent::KEYCODE_F3
+  #KbF4 = JavaImports::KeyEvent::KEYCODE_F4
+  #KbF5 = JavaImports::KeyEvent::KEYCODE_F5
+  #KbF6 = JavaImports::KeyEvent::KEYCODE_F6
+  #KbF7 = JavaImports::KeyEvent::KEYCODE_F7
+  #KbF8 = JavaImports::KeyEvent::KEYCODE_F8
+  #KbF9 = JavaImports::KeyEvent::KEYCODE_F9
   KbHome = JavaImports::KeyEvent::KEYCODE_HOME
-  KbInsert = JavaImports::KeyEvent::KEYCODE_INSERT
+  #KbInsert = JavaImports::KeyEvent::KEYCODE_INSERT
   KbLeft = JavaImports::KeyEvent::KEYCODE_DPAD_LEFT
   KbLeftAlt = JavaImports::KeyEvent::KEYCODE_ALT_LEFT
-  KbLeftControl = JavaImports::KeyEvent::KEYCODE_CTRL_LEFT
+  #KbLeftControl = JavaImports::KeyEvent::KEYCODE_CTRL_LEFT
   KbLeftShift = JavaImports::KeyEvent::KEYCODE_SHIFT_LEFT
-  KbNumpad0 = JavaImports::KeyEvent::KEYCODE_NUMPAD_0
-  KbNumpad1 = JavaImports::KeyEvent::KEYCODE_NUMPAD_1
-  KbNumpad2 = JavaImports::KeyEvent::KEYCODE_NUMPAD_2
-  KbNumpad3 = JavaImports::KeyEvent::KEYCODE_NUMPAD_3
-  KbNumpad4 = JavaImports::KeyEvent::KEYCODE_NUMPAD_4
-  KbNumpad5 = JavaImports::KeyEvent::KEYCODE_NUMPAD_5
-  KbNumpad6 = JavaImports::KeyEvent::KEYCODE_NUMPAD_6
-  KbNumpad7 = JavaImports::KeyEvent::KEYCODE_NUMPAD_7
-  KbNumpad8 = JavaImports::KeyEvent::KEYCODE_NUMPAD_8
-  KbNumpad9 = JavaImports::KeyEvent::KEYCODE_NUMPAD_9
-  KbNumpadAdd = JavaImports::KeyEvent::KEYCODE_NUMPAD_ADD
-  KbNumpadDivide = JavaImports::KeyEvent::KEYCODE_NUMPAD_DIVIDE
-  KbNumpadMultiply = JavaImports::KeyEvent::KEYCODE_NUMPAD_MULTIPLY
-  KbNumpadSubtract = JavaImports::KeyEvent::KEYCODE_NUMPAD_SUBTRACT
+  #KbNumpad0 = JavaImports::KeyEvent::KEYCODE_NUMPAD_0
+  #KbNumpad1 = JavaImports::KeyEvent::KEYCODE_NUMPAD_1
+  #KbNumpad2 = JavaImports::KeyEvent::KEYCODE_NUMPAD_2
+  #KbNumpad3 = JavaImports::KeyEvent::KEYCODE_NUMPAD_3
+  #KbNumpad4 = JavaImports::KeyEvent::KEYCODE_NUMPAD_4
+  #KbNumpad5 = JavaImports::KeyEvent::KEYCODE_NUMPAD_5
+  #KbNumpad6 = JavaImports::KeyEvent::KEYCODE_NUMPAD_6
+  #KbNumpad7 = JavaImports::KeyEvent::KEYCODE_NUMPAD_7
+  #KbNumpad8 = JavaImports::KeyEvent::KEYCODE_NUMPAD_8
+  #KbNumpad9 = JavaImports::KeyEvent::KEYCODE_NUMPAD_9
+  #KbNumpadAdd = JavaImports::KeyEvent::KEYCODE_NUMPAD_ADD
+  #KbNumpadDivide = JavaImports::KeyEvent::KEYCODE_NUMPAD_DIVIDE
+  #KbNumpadMultiply = JavaImports::KeyEvent::KEYCODE_NUMPAD_MULTIPLY
+  #KbNumpadSubtract = JavaImports::KeyEvent::KEYCODE_NUMPAD_SUBTRACT
   KbPageDown = JavaImports::KeyEvent::KEYCODE_PAGE_DOWN
   KbPageUp = JavaImports::KeyEvent::KEYCODE_PAGE_UP
   # Above the right shift key
   KbReturn = JavaImports::KeyEvent::KEYCODE_ENTER
   KbRight = JavaImports::KeyEvent::KEYCODE_DPAD_RIGHT
   KbRightAlt = JavaImports::KeyEvent::KEYCODE_ALT_RIGHT
-  KbRightControl = JavaImports::KeyEvent::KEYCODE_CTRL_RIGHT
+  #KbRightControl = JavaImports::KeyEvent::KEYCODE_CTRL_RIGHT
   KbRightShift = JavaImports::KeyEvent::KEYCODE_SHIFT_RIGHT
   KbSpace = JavaImports::KeyEvent::KEYCODE_SPACE
   KbTab = JavaImports::KeyEvent::KEYCODE_TAB
@@ -95,22 +97,22 @@ module Gosu
   #MsWheelDown = JavaImports::MotionEvent::AXIS_WHEEL
   #MsWheelUp = JavaImports::MotionEvent::AXIS_WHEEL
   #Game pad
-  GpButton0 = JavaImports::KeyEvent::KEYCODE_BUTTON_1
-  GpButton1 = JavaImports::KeyEvent::KEYCODE_BUTTON_2
-  GpButton10 = JavaImports::KeyEvent::KEYCODE_BUTTON_11
-  GpButton11 = JavaImports::KeyEvent::KEYCODE_BUTTON_12
-  GpButton12 = JavaImports::KeyEvent::KEYCODE_BUTTON_13
-  GpButton13 = JavaImports::KeyEvent::KEYCODE_BUTTON_14
-  GpButton14 = JavaImports::KeyEvent::KEYCODE_BUTTON_15
-  GpButton15 = JavaImports::KeyEvent::KEYCODE_BUTTON_16
-  GpButton2 = JavaImports::KeyEvent::KEYCODE_BUTTON_3
-  GpButton3 = JavaImports::KeyEvent::KEYCODE_BUTTON_4
-  GpButton4 = JavaImports::KeyEvent::KEYCODE_BUTTON_5
-  GpButton5 = JavaImports::KeyEvent::KEYCODE_BUTTON_6
-  GpButton6 = JavaImports::KeyEvent::KEYCODE_BUTTON_7
-  GpButton7 = JavaImports::KeyEvent::KEYCODE_BUTTON_8
-  GpButton8 = JavaImports::KeyEvent::KEYCODE_BUTTON_9
-  GpButton9 = JavaImports::KeyEvent::KEYCODE_BUTTON_10
+  #GpButton0 = JavaImports::KeyEvent::KEYCODE_BUTTON_1
+  #GpButton1 = JavaImports::KeyEvent::KEYCODE_BUTTON_2
+  #GpButton10 = JavaImports::KeyEvent::KEYCODE_BUTTON_11
+  #GpButton11 = JavaImports::KeyEvent::KEYCODE_BUTTON_12
+  #GpButton12 = JavaImports::KeyEvent::KEYCODE_BUTTON_13
+  #GpButton13 = JavaImports::KeyEvent::KEYCODE_BUTTON_14
+  #GpButton14 = JavaImports::KeyEvent::KEYCODE_BUTTON_15
+  #GpButton15 = JavaImports::KeyEvent::KEYCODE_BUTTON_16
+  #GpButton2 = JavaImports::KeyEvent::KEYCODE_BUTTON_3
+  #GpButton3 = JavaImports::KeyEvent::KEYCODE_BUTTON_4
+  #GpButton4 = JavaImports::KeyEvent::KEYCODE_BUTTON_5
+  #GpButton5 = JavaImports::KeyEvent::KEYCODE_BUTTON_6
+  #GpButton6 = JavaImports::KeyEvent::KEYCODE_BUTTON_7
+  #GpButton7 = JavaImports::KeyEvent::KEYCODE_BUTTON_8
+  #GpButton8 = JavaImports::KeyEvent::KEYCODE_BUTTON_9
+  #GpButton9 = JavaImports::KeyEvent::KEYCODE_BUTTON_10
   NoButton = 0xffffffff
   #Not Supported GpDown, GpLeft, GpRight, GpUp 
 end  
